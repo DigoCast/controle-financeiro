d4406f5db65f8b4b4a81217792f8ff21ead2ea45
DigoCast <diegocastilholorenceto@gmail.com>
2026-06-30 08:20:08 -0300

    feat: add gitignore and config setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI(
     title="Controle Financeiro API",
@@ -13,3 +14,6 @@ def read_root():
         "projeto": "Controle Financeiro",
         "grupo": "Grupo 8 - Diego & Davi"
     }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
